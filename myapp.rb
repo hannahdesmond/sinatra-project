e40346f89fadff41d70c2438d50b2bce94336be8
Hannah Desmond <desmond.hannah@googlemail.com>
2021-02-16 15:23:04 +0000

    Create a route called /named-cat'

--- myapp.rb
+++ myapp.rb
@@ -9,7 +9,13 @@ get '/secret' do
   'This is a secret page'
 end
 
-get '/cat' do
+get '/random-cat' do
   @cat_name = ["Randall", "Minnie", "Susan"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @cat_name = params[:name]
   erb(:index)
 end
